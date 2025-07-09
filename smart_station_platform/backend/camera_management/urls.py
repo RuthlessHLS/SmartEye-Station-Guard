0511b0bc15aee0cc6d4e3c5b8eb3513de5dc18c4
Grammar0201 <2670765754@qq.com>
2025-07-09 15:44:12 +0800

    camera的定位
    我累死了

--- smart_station_platform/backend/camera_management/urls.py
+++ smart_station_platform/backend/camera_management/urls.py
@@ -6,3 +6,19 @@ from . import views
 urlpatterns = [
     # your specific urls for this app
 ]
+
+
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
+
+# 创建路由器
+router = DefaultRouter()
+router.register(r'cameras', views.CameraViewSet, basename='camera')
+router.register(r'dangerous-areas', views.DangerousAreaViewSet, basename='dangerous-area')
+
+urlpatterns = [
+    # 包含路由器生成的URL
+    path('', include(router.urls)),
+]
--- smart_station_platform/backend/smart_station/urls.py
+++ smart_station_platform/backend/smart_station/urls.py
@@ -9,6 +9,7 @@ from rest_framework_simplejwt.views import (
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from django.shortcuts import redirect
 
